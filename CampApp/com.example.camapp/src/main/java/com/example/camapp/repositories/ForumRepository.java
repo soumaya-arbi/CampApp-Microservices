97ed7a563f8463d290624587e6805f5e481586b5
BotSpontex <skander.safer@gmail.com>
2025-07-04 21:46:17 +0200

    skander

---
+++ CampApp/com.example.camapp/src/main/java/com/example/camapp/repositories/ForumRepository.java
@@ -0,0 +1,9 @@
+package com.example.camapp.repositories;
+
+import com.example.camapp.entities.Forum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ForumRepository extends JpaRepository<Forum, Long> {
+}
